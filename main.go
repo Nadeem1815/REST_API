837b311262fbccd429700f652c24f1c18a0bec28
Nadeem1815 <nadeemf408@gmail.com>
2025-12-05 19:12:59 +0530

    server is done

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/events", getEvents) //GET, POST, PATH, DELETE
+
+	server.Run(":8080") // localhost:8080
+}
+
+func getEvents(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "Hello!",
+	})
+}
