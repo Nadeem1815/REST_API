219d260dfdae04e2bff4481ab239d3c3f6e9931a
Nadeem1815 <nadeemf408@gmail.com>
2025-12-07 15:53:46 +0530

    create and getall seccess

--- main.go
+++ main.go
@@ -3,19 +3,43 @@ package main
 import (
 	"net/http"
 
+	"github.com/Nadeem1815/rest-api/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	server := gin.Default()
 
-	server.GET("/events", getEvents) //GET, POST, PATH, DELETE
+	server.GET("/events", GetEvents) //GET, POST, PATH, DELETE
+	server.POST("/events", CreateEvent)
 
 	server.Run(":8080") // localhost:8080
 }
 
-func getEvents(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "Hello!",
+func GetEvents(ctx *gin.Context) {
+	events := models.GetAllEvents()
+	ctx.JSON(http.StatusOK, events)
+}
+
+func CreateEvent(ctx *gin.Context) {
+	var event models.Event
+
+	err := ctx.BindJSON(&event)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"massage": "could not parse the data",
+		})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	event.Save()
+	ctx.JSON(http.StatusCreated, gin.H{
+		"massage": "event created",
+		"event":   event,
 	})
+
 }
